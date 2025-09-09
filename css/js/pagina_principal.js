2840da92696efc110aeb7efb2abc882c8c19bbe6
Taislopo <115121419+Taislopo@users.noreply.github.com>
2025-09-09 00:51:14 -0300

    quaselar

--- css/js/pagina_principal.js
+++
@@ -1,4 +0,0 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navbar = document.querySelector(".navbar");
-
-hamburguer.addEventListener("click", () => navbar.classList.toggle("active"));
